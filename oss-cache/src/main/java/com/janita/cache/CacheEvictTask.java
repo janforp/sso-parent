79029cd07ebd8b3edfe2768f5034d7a9ab91c6de
Janita <zhuchenjian@huizhi-tech.com>
2017-04-01 11:35:17 +0800

    cache

---
+++ oss-cache/src/main/java/com/janita/cache/CacheEvictTask.java
@@ -0,0 +1,24 @@
+package com.janita.cache;
+
+
+import com.janita.cache.strategy.CacheStrategy;
+
+import java.util.TimerTask;
+
+/**
+ * 缓存清除任务
+ */
+public class CacheEvictTask extends TimerTask {
+    private final CacheStrategy cacheStrategy; //缓存策略
+    private final Object key; //要清除的键
+
+    public CacheEvictTask(CacheStrategy cacheStrategy, Object key) {
+        this.cacheStrategy = cacheStrategy;
+        this.key = key;
+    }
+
+    @Override
+    public void run() {
+        cacheStrategy.evict(key);
+    }
+}
